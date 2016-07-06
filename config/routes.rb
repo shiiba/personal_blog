9834c10b0770867f92a41e2f30459644b8b2f305
Shiiba <kshiiba@gmail.com>
2016-07-06 13:48:45 -0400

    adding first and last name to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 end
+
+#                   Prefix Verb   URI Pattern                    Controller#Action
+#         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)       devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+#            user_password POST   /users/password(.:format)      devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+#                          PATCH  /users/password(.:format)      devise/passwords#update
+#                          PUT    /users/password(.:format)      devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)        registrations#cancel
+#        user_registration POST   /users(.:format)               registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)       registrations#new
+#   edit_user_registration GET    /users/edit(.:format)          registrations#edit
+#                          PATCH  /users(.:format)               registrations#update
+#                          PUT    /users(.:format)               registrations#update
+#                          DELETE /users(.:format)               registrations#destroy

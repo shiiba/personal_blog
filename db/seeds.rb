3e6f34741a9d0eb3ea44f3d5e8729f1681cad27e
Shiiba <kshiiba@gmail.com>
2016-07-18 12:06:29 -0400

    styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,20 @@ Post.delete_all
 
 blog_posts = Post.create([
   {
-    title: "My First Post",
-    body: "Hodor! Hodor hodor, hodor hodor, hodor. Hodor hodor. Hodor, hodor. Hodor. Hodor, hodor; hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor... Hodor hodor hodor... Hodor hodor hodor?! Hodor hodor HODOR! Hodor hodor, hodor. Hodor hodor; hodor hodor hodor. Hodor hodor HODOR! Hodor HODOR hodor, hodor hodor hodor, hodor, hodor hodor. Hodor hodor - hodor; hodor hodor. Hodor. Hodor. Hodor hodor hodor hodor; hodor hodor - hodor. Hodor hodor - hodor hodor HODOR hodor, hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - hodor hodor; hodor hodor?"
+    title: "Hodor on War...",
+    body: "Hodor! Hodor hodor, hodor hodor, hodor. Hodor hodor. Hodor, hodor. Hodor. Hodor, hodor; hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor... Hodor hodor hodor... Hodor hodor hodor?! Hodor hodor HODOR! Hodor hodor, hodor. Hodor hodor; hodor hodor hodor. Hodor hodor HODOR! Hodor HODOR hodor, hodor hodor hodor, hodor, hodor hodor. Hodor hodor - hodor; hodor hodor. Hodor. Hodor. Hodor hodor hodor hodor; hodor hodor - hodor. Hodor hodor - hodor hodor HODOR hodor, hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - hodor hodor; hodor hodor?",
+    published: true
   },{
-    title: "My Second Post",
-    body: "Hodor hodor - hodor... Hodor hodor hodor hodor hodor! Hodor, hodor. Hodor. Hodor, hodor hodor hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor; hodor HODOR hodor, hodor hodor, hodor. Hodor hodor. Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor. Hodor, hodor - hodor hodor hodor! Hodor hodor - hodor - hodor... Hodor hodor hodor hodor. Hodor, hodor - hodor. Hodor. Hodor hodor - hodor... Hodor hodor hodor - hodor?! Hodor hodor - hodor, hodor. Hodor hodor, hodor. Hodor hodor hodor hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, HODOR hodor, hodor hodor."
-  } 
+    title: "Hodor on Racism...",
+    body: "Hodor hodor - hodor... Hodor hodor hodor hodor hodor! Hodor, hodor. Hodor. Hodor, hodor hodor hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor; hodor HODOR hodor, hodor hodor, hodor. Hodor hodor. Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor. Hodor, hodor - hodor hodor hodor! Hodor hodor - hodor - hodor... Hodor hodor hodor hodor. Hodor, hodor - hodor. Hodor. Hodor hodor - hodor... Hodor hodor hodor - hodor?! Hodor hodor - hodor, hodor. Hodor hodor, hodor. Hodor hodor hodor hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, HODOR hodor, hodor hodor.",
+    published: true
+  },{
+    title: "Hodor on the meaning of life...",
+    body: "Hodor hodor hodor. Hodor hodor - HODOR hodor, hodor hodor, hodor. Hodor hodor. Hodor. Hodor, hodor... Hodor hodor hodor hodor hodor hodor hodor - hodor. Hodor. Hodor, hodor. Hodor. Hodor, HODOR hodor, hodor hodor... Hodor hodor hodor?! Hodor hodor HODOR! Hodor HODOR hodor, hodor hodor - hodor hodor hodor?! Hodor hodor hodor hodor... Hodor hodor hodor... Hodor hodor hodor. Hodor. Hodor hodor, hodor. Hodor hodor; hodor hodor hodor?",
+    published: true
+  },{
+    title: "Hodor on how he got his name...",
+    body: "Hodor, hodor, hodor. Hodor hodor, hodor. Hodor HODOR hodor, hodor hodor. Hodor. Hodor HODOR hodor, hodor hodor... Hodor hodor HODOR hodor, hodor hodor?! Hodor hodor - hodor; hodor HODOR hodor, hodor hodor... Hodor hodor hodor hodor! Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor hodor hodor, hodor, hodor hodor. Hodor, HODOR hodor, hodor hodor hodor hodor? Hodor hodor; hodor hodor - hodor; hodor HODOR hodor, hodor hodor. Hodor hodor, hodor. Hodor HODOR hodor, hodor hodor.",
+    published: true
+  }
 ])
